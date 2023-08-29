3307369ce53a203a9c78fe6d1fdaf3c5b2a99bee
aaveggupta <aaveggupta.contact@gmail.com>
2023-08-29 21:42:22 +0530

    29-08-2023

--- src/components/UseStateHook/UseStateHook.jsx
+++ src/components/UseStateHook/UseStateHook.jsx
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import "./UseStateHook.css";
 
 const UseStateHook = () => {
-  const [count, setCount] = useState(0); // [state, function - state modify]
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  // usestate do cheeze return (array ki form mein kr rha) array
+  // ka pehle -> var, array ka dusara > (fun, var ko change krne k kaam aaega)
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  console.log(count, "OUTSIDE");
+
   return (
-    <div className="useeffecthook">
+    <div className="usestatehook">
       <button onClick={decrement}>-</button>
       <h1>{count}</h1>
       <button onClick={increment}>+</button>
